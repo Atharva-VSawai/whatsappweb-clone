37b5694f1e248ef3235f3799c03bfa39d7f0512d
Atharva_Sawai <atharva.s.official@gmail.com>
2025-09-08 01:26:22 +0530

    Chatlist and Fronted Connected to Backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/user-login/Login";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { ProtectedRoute, PublicRoute } from "./Protected";
+import HomePage from "./components/HomePage";
+import UserDetails from "./components/UserDetails";
+import Status from "./pages/statusSection/Status";
+import Setting from "./pages/settingSection/Setting";
 
 function App() {
   return (
-    <div className="bg-black text-white">
-      Hello Atharva
-    </div>
+    <>
+      <ToastContainer position="top-right" autoClose={3000} />
+      <Router>
+        <Routes>
+          <Route element={<PublicRoute />}>
+            <Route path="/user-login" element={<Login />} />
+          </Route>
+
+          <Route element={<ProtectedRoute />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/user-profile" element={<UserDetails />} />
+            <Route path="/status" element={<Status />} />
+            <Route path="/setting" element={<Setting />} />
+          </Route>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
